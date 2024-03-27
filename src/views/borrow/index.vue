52641ef9d498cd56fd8ea342d8ddefd3dd4b3469
peipi <2390574106@qq.com>
2024-03-27 18:13:23 +0800

    shenhe

--- src/views/borrow/index.vue
+++ src/views/borrow/index.vue
@@ -1,11 +1,29 @@
 <template>
-    <div>审核管理</div>
+    <VaTabs v-model="value">
+    <template #tabs>
+      <VaTab
+        v-for="(tab,index) in tabs"
+        :key="index"
+      >
+        {{ tab }}
+      </VaTab>
+    </template>
+  </VaTabs>
+  <!-- 借阅审核页面内容 -->
+  <div v-if="value === 0">
+
+</div>
+  <!-- 归还审核页面内容 -->
+  <div v-if="value === 1">
+    
+  </div>
 </template>
 
-<script>
-export default{
-    name:''
-}
+<script setup>
+import {ref} from 'vue';
+const value = ref(0);
+const tabs = ['借阅审核','归还审核']
+
 </script>
 
 <style scoped>
